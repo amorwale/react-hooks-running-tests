8dc5c340ae728ff733fa21f0d5eccb198ba9334b
Ashay Morwale <ashay123.m@gmail.com>
2023-05-02 01:05:08 -0400

    complete lab

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,8 @@ import React from "react";
 
 function Article() {
   return (
-    <div>
-      {/* please pass this test */}
-    </div>
-  );
+    <div>please pass this test</div>
+  )
 }
 
 export default Article;
